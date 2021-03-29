2fd016984a357d612903d17421945c2c3d2bdb96
2586224896 <2586224896@qq.com>
2021-03-29 21:48:49 +0800

    add html5 drag

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,65 +1,82 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-pwa" target="_blank" rel="noopener">pwa</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router" target="_blank" rel="noopener">router</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-vuex" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-unit-mocha" target="_blank" rel="noopener">unit-mocha</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-typescript" target="_blank" rel="noopener">typescript</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+<div class="hello">
+    <div class="d1"
+         id="d1"></div>
+    <div class="d2"
+         id="d2"></div>
+</div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, onMounted, ref } from 'vue';
 
 export default defineComponent({
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-})
+    name: 'HelloWorld',
+    props: {
+        msg: String
+    },
+    data() {
+        return {
+        };
+    },
+    setup() {
+        // console.log(1212);
+        let offsetX = ref(0);
+        let offsetY = ref(0);
+        onMounted(() => {
+            const dd = document.getElementById('d1') as any;
+            dd.ondragstart = (e: any) => {
+                console.log(e);
+                offsetX = e.offsetX;
+                offsetY = e.offsetY;
+            };
+            dd.ondrag = (e: any) => {
+                const x = e.pageX;
+                const y = e.pageY;
+                console.log('e', e);
+                console.log(x, y);
+                // if (x == 0 && y == 0) {
+                //     return;
+                // }
+                // x -= offsetX.value;
+                // y -= offsetY.value;
+                dd.style.left = x + 'px';
+                dd.style.top = y + 'px';
+            };
+            dd.ondragend = (e: any) => {
+                const x = e.pageX;
+                const y = e.pageY;
+                dd.style.left = x + 'px';
+                dd.style.top = y + 'px';
+            };
+        });
+        return {
+            offsetX,
+            offsetY
+        };
+    }
+});
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+<style lang="scss" scoped>
+.hello {
+    width: 100%;
+    height: 100%;
+    position: relative;
+    border: 1px solid #ececec;
+    .d1 {
+        width: 150px;
+        height: 150px;
+        background: blueviolet;
+        left:0;
+        top:0;
+        position: absolute;
+    }
+
+    .d2 {
+        width: 500px;
+        height: 500px;
+        background: cadetblue
+    }
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,21 +1,31 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
-  </div>
+<div class="home">
+    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+</div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
-import HelloWorld from '@/components/HelloWorld.vue' // @ is an alias to /src
+import { defineComponent } from 'vue';
+import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
 
 export default defineComponent({
-  name: 'Home',
-  components: {
-    HelloWorld
-  },
-  methods: {
+    name: 'Home',
+    components: {
+        HelloWorld
+    },
+    mounted() {
+        // console.log(0);
+        // alert(1);
+    },
+    methods: {
 
-  }
-})
+    }
+});
 </script>
+
+<style lang="scss">
+.home {
+    width: 100%;
+    height: 100%;
+}
+</style>
