217d8302bb2070e23a0e5f855b416aaffeb919a8
2586224896 <2586224896@qq.com>
2021-03-30 22:07:43 +0800

    drag

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,10 +1,10 @@
 <template>
-<div class="hello">
-    <div class="d1"
-    draggable="true"
-         id="d1"></div>
-    <div class="d2"
-         id="d2"></div>
+<div class="drag-page">
+    <div class="orgEl"
+         id="orgEl"
+         draggable="true"></div>
+    <!-- <div class="tagEl"
+         id="tagEl"></div> -->
 </div>
 </template>
 
@@ -18,37 +18,34 @@ export default defineComponent({
     },
     data() {
         return {
+
         };
     },
     setup() {
-        // console.log(1212);
         let offsetX = ref(0);
         let offsetY = ref(0);
         onMounted(() => {
-            const dd = document.getElementById('d1') as any;
-            dd.ondragstart = (e: any) => {
-                console.log(e);
+            const orgEl = document.getElementById('orgEl') as any;
+            orgEl.ondragstart = (e: any) => {
                 offsetX = e.offsetX;
                 offsetY = e.offsetY;
             };
-            dd.ondrag = (e: any) => {
-                const x = e.pageX;
-                const y = e.pageY;
-                console.log('e', e);
-                console.log(x, y);
-                // if (x == 0 && y == 0) {
-                //     return;
-                // }
-                // x -= offsetX.value;
-                // y -= offsetY.value;
-                dd.style.left = x + 'px';
-                dd.style.top = y + 'px';
+            orgEl.ondrag = (e: any) => {
+                let x = e.pageX;
+                let y = e.pageY;
+                if (x == 0 && y == 0) {
+                    return;
+                }
+                x -= offsetX.value;
+                y -= offsetY.value;
+                orgEl.style.left = x + 'px';
+                orgEl.style.top = y + 'px';
             };
-            dd.ondragend = (e: any) => {
+            orgEl.ondragend = (e: any) => {
                 const x = e.pageX;
                 const y = e.pageY;
-                dd.style.left = x + 'px';
-                dd.style.top = y + 'px';
+                orgEl.style.left = x + 'px';
+                orgEl.style.top = y + 'px';
             };
         });
         return {
@@ -60,21 +57,22 @@ export default defineComponent({
 </script>
 
 <style lang="scss" scoped>
-.hello {
+.drag-page {
     width: 100%;
     height: 100%;
     position: relative;
     border: 1px solid #ececec;
-    .d1 {
+
+    .orgEl {
         width: 150px;
         height: 150px;
         background: blueviolet;
-        left:0;
-        top:0;
+        left: 0;
+        top: 0;
         position: absolute;
     }
 
-    .d2 {
+    .tagEl {
         width: 500px;
         height: 500px;
         background: cadetblue
